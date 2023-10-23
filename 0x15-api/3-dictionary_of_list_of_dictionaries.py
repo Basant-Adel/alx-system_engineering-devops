d9f53f34bbb7f2ec8c1d6c4bcbb3d58dfae60de4
Basant-Adel <basantadel99@gmail.com>
2023-10-23 13:20:06 -0700

    Dictionary of list :D

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     URL = 'https://jsonplaceholder.typicode.com'
     ulist = requests.get(f'{URL}/users').json()
     tlist = requests.get(f'{URL}/todos').json()
-    alltodoemp = {}
+    todo_all_employees = {}
     for user in ulist:
         user_id = user.get('id')
         emplist = []
@@ -20,6 +20,6 @@ if __name__ == "__main__":
                                  "task": task.get('title'),
                                  "completed": task.get('completed')
                                  })
-        alltodoemp[user_id] = emplist
-    with open("alltodoemp.json", "w") as outfile:
-        json.dump(alltodoemp, outfile)
+        todo_all_employees[user_id] = emplist
+    with open("todo_all_employees.json", "w") as outfile:
+        json.dump(todo_all_employees, outfile)
